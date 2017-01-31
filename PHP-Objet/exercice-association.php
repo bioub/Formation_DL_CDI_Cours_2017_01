1cdf913da04c4e3d5deb2b2916c26b102974114c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-01-31 11:43:35 +0100

    Enoncé exercice

---
+++ PHP-Objet/exercice-association.php
@@ -0,0 +1,21 @@
+<?php
+
+// 1 - Créer un Contact
+// Créer un compte bancaire
+// faire du Contact le propriétaire du compte bancaire
+
+/*
+...
+ */
+
+// 2 - Adapter la classe voiture en encapsulant les propriétés
+// (les rendres protected) et en créant les getters/setters
+
+// 3 - Faire le lien entre Contact et Voiture côté Voiture
+// (relation propriétaire) et faire un petit exemple
+
+// 4 - Créer la classe Groupe (groupe de Contact)
+// avec les propriétés nom et description
+// et lier les Groupes aux contacts côté Groupe et faire
+// un petit exemple
+
